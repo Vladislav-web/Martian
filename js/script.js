86ec3f57e3ec7b1963cb8d71d7b548059513dabc
Vladislav-web <ykt.pc_master@mail.ru>
2020-12-08 15:39:35 +0300

    added Modal

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
-//! Tabs
 window.addEventListener('DOMContentLoaded', () => {
+
+  //! Tabs
 
   const tabs = document.querySelectorAll('.tabheader__item'),
     tabsContent = document.querySelectorAll('.tabcontent'),
@@ -39,13 +40,14 @@ window.addEventListener('DOMContentLoaded', () => {
   });
 
   //! Timer
-  const deadline = '2020-12-16 04:06';
+
+  const deadline = '2020-12-15 20:19';
 
   function getTimeRemaining(endtime) {
     const t = Date.parse(endtime) - Date.parse(new Date()),
       days = Math.floor(t / (1000 * 60 * 60 * 24)),
-      hours = Math.floor((t / (1000 * 60 * 60) % 24)),
-      minutes = Math.floor((t / 1000 / 60) % 60),
+      hours = Math.floor(t / (1000 * 60 * 60) % 24),
+      minutes = Math.floor(t / (1000 * 60) % 60),
       seconds = Math.floor((t / 1000) % 60);
 
     return {
@@ -53,7 +55,7 @@ window.addEventListener('DOMContentLoaded', () => {
       'days': days,
       'hours': hours,
       'minutes': minutes,
-      'seconds': seconds
+      'seconds': seconds,
     };
   }
 
@@ -67,7 +69,6 @@ window.addEventListener('DOMContentLoaded', () => {
     }
   }
 
-
   function setClock(selector, endtime) {
     const timer = document.querySelector(selector),
       days = timer.querySelector('#days'),
@@ -75,9 +76,9 @@ window.addEventListener('DOMContentLoaded', () => {
       minutes = timer.querySelector('#minutes'),
       seconds = timer.querySelector('#seconds'),
       timeInterval = setInterval(updateClock, 1000);
-    
+
     updateClock();
-    
+
     function updateClock() {
       const t = getTimeRemaining(endtime);
 
@@ -88,71 +89,124 @@ window.addEventListener('DOMContentLoaded', () => {
 
       if (t.total <= 0) {
         clearInterval(timeInterval);
+        document.querySelectorAll('.clock-title').forEach(item => {
+          item.textContent = 'Акция закончилась!!!';
+          item.style.color = 'red';
+          item.style.fontWeight = '700';
+        });
       }
     }
   }
 
   setClock('.timer', deadline);
+
+  //! Modal
+
+  const modalTrigger = document.querySelectorAll('[data-modal]'),
+    modal = document.querySelector('.modal'),
+    modalCloseBtn = document.querySelector('[data-close]');
+
+  modalTrigger.forEach(btn => {
+    btn.addEventListener('click', () => {
+      modal.classList.add('show');
+      modal.classList.remove('hide');
+      document.body.style.overflow = 'hidden';
+    });
+  });
+
+  function closeModal(params) {
+      modal.classList.add('hide');
+      modal.classList.remove('show');
+      document.body.style.overflow = '';
+  }
+
+  modalCloseBtn.addEventListener('click', closeModal);
+
+  modal.addEventListener('click', (e) => {
+    if (e.target === modal) {
+      closeModal();
+    }
+  });
+
+  document.addEventListener('keydown', (e) => {
+    if (e.code === "Escape" && modal.classList.contains('show')) {
+      closeModal(); 
+    }
+  });
 });
 
 
-  // //! Timer
-  // const deadline = '2020-12-16 04:06';
-
-  // function getTimeRemaining(endtime) {
-  //   const t = Date.parse(endtime) - Date.parse(new Date()),
-  //     days = Math.floor(t / (1000 * 60 * 60 * 24)),
-  //     hours = Math.floor((t / (1000 * 60 * 60) % 24)),
-  //     minutes = Math.floor((t / 1000 / 60) % 60),
-  //     seconds = Math.floor((t / 1000) % 60);
-
-  //   return {
-  //     'total': t,
-  //     'days': days,
-  //     'hours': hours,
-  //     'minutes': minutes,
-  //     'seconds': seconds
-  //   };
-  // }
-
-  // function getZero(num) {
-  //   if (num >= 0 && num < 10) {
-  //     return `0${num}`;
-  //   } else if (num <= 0) {
-  //     return `00`;
-  //   } else {
-  //     return num;
-  //   }
-  // }
 
 
-  // function setClock(selector, endtime) {
-  //   const timer = document.querySelector(selector),
-  //     days = timer.querySelector('#days'),
-  //     hours = timer.querySelector('#hours'),
-  //     minutes = timer.querySelector('#minutes'),
-  //     seconds = timer.querySelector('#seconds'),
-  //     timeInterval = setInterval(updateClock, 1000);
 
-  //   updateClock();
 
-  //   function updateClock() {
-  //     const t = getTimeRemaining(endtime);
 
-  //     days.innerHTML = getZero(t.days);
-  //     hours.innerHTML = getZero(t.hours);
-  //     minutes.innerHTML = getZero(t.minutes);
-  //     seconds.innerHTML = getZero(t.seconds);
 
-  //     if (t.total <= 0) {
-  //       clearInterval(timeInterval);
-  //     }
-  //   }
-  // }
 
-  // setClock('.timer', deadline);
-  // });
 
+
+
+
+// //! Timer
+// const deadline = '2020-12-16 23:59';
+
+// function getTimeRemaining(endtime) {
+//   const t = Date.parse(endtime) - Date.parse(new Date()),
+//     days = Math.floor(t / (1000 * 60 * 60 * 24)),
+//     hours = Math.floor(t / (1000 * 60 * 60) % 24),
+//     minutes = Math.floor(t / (1000 * 60) % 60),
+//     seconds = Math.floor((t / 1000) % 60);
+
+//   return {
+//     'total': t,
+//     'days': days,
+//     'hours': hours,
+//     'minutes': minutes,
+//     'seconds': seconds
+//   };
+// }
+
+// function getZero(num) {
+//   if (num >= 0 && num < 10) {
+//     return `0${num}`;
+//   } else if (num <= 0) {
+//     return `00`;
+//   } else {
+//     return num;
+//   }
+// }
+
+
+// function setClock(selector, endtime) {
+//   const timer = document.querySelector(selector),
+//     days = timer.querySelector('#days'),
+//     hours = timer.querySelector('#hours'),
+//     minutes = timer.querySelector('#minutes'),
+//     seconds = timer.querySelector('#seconds'),
+//     timeInterval = setInterval(updateClock, 1000);
+
+//   updateClock();
+
+//   function updateClock() {
+//     const t = getTimeRemaining(endtime);
+
+//     days.innerHTML = getZero(t.days);
+//     hours.innerHTML = getZero(t.hours);
+//     minutes.innerHTML = getZero(t.minutes);
+//     seconds.innerHTML = getZero(t.seconds);
+
+//     if (t.total <= 0) {
+//       clearInterval(timeInterval);
+//       document.querySelectorAll('.clock-title').forEach(item => {
+//         item.textContent = 'Акция закончилась!!!';
+//         item.style.color = 'red';
+//         item.style.fontWeight = '700';
+//       });
+//     }
+//   }
+// }
+
+// setClock('.timer', deadline);
 
 //! Tabs
 // window.addEventListener('DOMContentLoaded', () => {
