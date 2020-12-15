c91ab915f74cb11fdf0fc228f3f34d968cbe2da2
Vladislav-web <ykt.pc_master@mail.ru>
2020-12-15 21:03:20 +0300

    added class

--- js/script.js
+++ js/script.js
@@ -43,7 +43,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
   //! Timer
 
-  const deadline = '2020-12-15 20:19';
+  const deadline = '2020-12-25 20:19';
 
   function getTimeRemaining(endtime) {
     const t = Date.parse(endtime) - Date.parse(new Date()),
@@ -107,23 +107,32 @@ window.addEventListener('DOMContentLoaded', () => {
   const modalTrigger = document.querySelectorAll('[data-modal]'),
     modal = document.querySelector('.modal'),
     modalCloseBtn = document.querySelector('[data-close]');
+  // modalTimerId = setTimeout(openModal, 5000);
 
   function openModal() {
     modal.classList.add('show');
     modal.classList.remove('hide');
     document.body.style.overflow = 'hidden';
-    clearInterval(modalTimerId);
+    // clearTimeout(modalTimerId);
   }
-  
-  modalTrigger.forEach(btn => {
-    btn.addEventListener('click', openModal);
-  });
 
-  function closeModal(params) {
+  function closeModal() {
     modal.classList.add('hide');
     modal.classList.remove('show');
     document.body.style.overflow = '';
   }
+
+  function showModalByScroll() {
+    if (window.pageYOffset + document.documentElement.clientHeight >=
+      document.documentElement.scrollHeight) {
+      openModal();
+      window.removeEventListener('scroll', showModalByScroll);
+    }
+  }
+
+  modalTrigger.forEach(item => {
+    item.addEventListener('click', openModal);
+  });
 
   modalCloseBtn.addEventListener('click', closeModal);
 
@@ -139,21 +148,123 @@ window.addEventListener('DOMContentLoaded', () => {
     }
   });
 
-  const modalTimerId = setTimeout(openModal, 5000);
-  
-  function showModalByScroll(params) {
-    if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
-      openModal();
-      window.removeEventListener('scroll', showModalByScroll);
+  window.addEventListener('scroll', showModalByScroll);
+
+  //!! Используем классы для карточек
+
+  class MenuCard {
+    constructor(src, alt, title, descr, price, parentSelector) {
+      this.src = src;
+      this.alt = alt;
+      this.title = title;
+      this.descr = descr;
+      this.price = price;
+      this.parent = document.querySelector(parentSelector);
+      this.transfer = 27;
+      this.changeToUAH();
+    }
+
+    changeToUAH() {
+      this.price = this.price * this.transfer;
+    }
+
+    render() {
+      const element = document.createElement('div');
+      element.innerHTML = `
+      <div class = "menu__item">
+            <img src =${this.src} alt =${this.alt}>
+            <h3 class = "menu__item-subtitle">${this.title}</h3>
+            <div class = "menu__item-descr">${this.descr}</div> 
+            <div class = "menu__item-divider"></div>
+            <div class = "menu__item-price">
+                  <div class = "menu__item-cost"> Цена: </div>
+                  <div class = "menu__item-total"><span>${this.price}</span> грн/день </div>
+            </div> 
+      </div>
+      `;
+      this.parent.append(element);
     }
   }
 
-  window.addEventListener('scroll', showModalByScroll);
+  new MenuCard(
+    "img/tabs/vegy.jpg",
+    "vegy",
+    'Меню "Фитнес"',
+    'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+    9, 
+    '.menu .container'
+  ).render();
+
+  new MenuCard(
+    "img/tabs/elite.jpg",
+    "elite",
+    'Меню "Премиум"',
+    'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+    14, 
+    '.menu .container'
+  ).render();
+
+  new MenuCard(
+    "img/tabs/post.jpg",
+    "post",
+    'Меню "Постное"',
+    'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков. ',
+    21, 
+    '.menu .container'
+  ).render();
 });
 
 
+//! Modal NEW 
 
-// //! Modal Старый!!!
+// const modalTrigger = document.querySelectorAll('[data-modal]'),
+//   modal = document.querySelector('.modal'),
+//   modalCloseBtn = document.querySelector('[data-close]'),
+//   modalTimerId = setTimeout(openModal, 5000);
+
+// function openModal() {
+//   modal.classList.add('show');
+//   modal.classList.remove('hide');
+//   document.body.style.overflow = 'hidden';
+//   clearInterval(modalTimerId);
+// }
+
+// function closeModal() {
+//   modal.classList.add('hide');
+//   modal.classList.remove('show');
+//   document.body.style.overflow = '';
+// }
+
+
+// function showModalByScroll() {
+//   if (window.pageYOffset + document.documentElement.clientHeight >= 
+//    document.documentElement.scrollHeight) {
+//     openModal();
+//     window.removeEventListener('scroll', showModalByScroll);
+//   }
+// }
+
+// modalTrigger.forEach(btn => {
+//   btn.addEventListener('click', openModal);
+// });
+
+// modalCloseBtn.addEventListener('click', closeModal);
+
+// modal.addEventListener('click', (e) => {
+//   if (e.target === modal) {
+//     closeModal();
+//   }
+// });
+
+// document.addEventListener('keydown', (e) => {
+//   if (e.code === "Escape" && modal.classList.contains('show')) {
+//     closeModal();
+//   }
+// });
+
+// window.addEventListener('scroll', showModalByScroll);
+
+//! Modal Старый
 
 // const modalTrigger = document.querySelectorAll('[data-modal]'),
 //   modal = document.querySelector('.modal'),
